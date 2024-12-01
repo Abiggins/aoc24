8b562d3ea08a6d7d140a440a42e68d54a6026ee9
Abiggins <andrewbigginsj@Gmail.com>
2024-12-01 17:22:36 +0000

    day 1 part 1

---
+++ day1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace day1
+{
+    class ListDistances
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            List<int> leftList = new List<int>();
+            List<int> rightList = new List<int>();
+            List<int> differences = new List<int>();
+
+            string line;
+
+            while((line = sr.ReadLine()) != null)
+            {
+                leftList.Add(int.Parse(line.Split("   ")[0]));
+                rightList.Add(int.Parse(line.Split("   ")[1]));
+            }
+            
+            leftList.Sort();
+            rightList.Sort();
+
+            int sum = 0;
+            for (int i = 0; i < leftList.Count; i++)
+            {
+                sum += Math.Abs(leftList[i] - rightList[i]);
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
