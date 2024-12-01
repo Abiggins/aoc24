b1035a26cea97110c9b7ef8926117b71997d27bb
Abiggins <andrewbigginsj@Gmail.com>
2024-12-01 17:43:39 +0000

    day1 part2

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 
 namespace day1
@@ -12,15 +13,16 @@ namespace day1
             List<int> leftList = new List<int>();
             List<int> rightList = new List<int>();
             List<int> differences = new List<int>();
+            List<int> probabilityList = new List<int>();
 
             string line;
 
-            while((line = sr.ReadLine()) != null)
+            while ((line = sr.ReadLine()) != null)
             {
                 leftList.Add(int.Parse(line.Split("   ")[0]));
                 rightList.Add(int.Parse(line.Split("   ")[1]));
             }
-            
+
             leftList.Sort();
             rightList.Sort();
 
@@ -31,6 +33,25 @@ namespace day1
             }
 
             Console.WriteLine(sum);
+
+            int probabilityScore = 0;
+            foreach (int leftNum in leftList)
+            {
+                int count = 0;
+                int value = 0;
+                foreach (int rightNum in rightList)
+                {
+                    if (leftNum == rightNum)
+                    {
+                        count++;
+                    }
+                }
+                value = leftNum * count;
+                probabilityScore += value;
+            }
+
+            Console.WriteLine(probabilityScore);
+
         }
     }
 }
--- day1/obj/Debug/net9.0/day1.AssemblyInfo.cs
+++ day1/obj/Debug/net9.0/day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b562d3ea08a6d7d140a440a42e68d54a6026ee9")]
 [assembly: System.Reflection.AssemblyProductAttribute("day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
