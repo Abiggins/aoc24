ab92c9daa19fecc3f9f698379864b932bf0c927c
Abiggins <andrewbigginsj@Gmail.com>
2024-12-01 18:16:17 +0000

    turns out i didnt need any of the class shit

--- day1/Program.cs
+++ day1/Program.cs
@@ -3,55 +3,45 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 
-namespace day1
+StreamReader sr = new StreamReader("input.txt");
+List<int> leftList = new List<int>();
+List<int> rightList = new List<int>();
+List<int> differences = new List<int>();
+List<int> probabilityList = new List<int>();
+
+string line;
+
+while ((line = sr.ReadLine()) != null)
 {
-    class ListDistances
+    leftList.Add(int.Parse(line.Split("   ")[0]));
+    rightList.Add(int.Parse(line.Split("   ")[1]));
+}
+
+leftList.Sort();
+rightList.Sort();
+
+int sum = 0;
+for (int i = 0; i < leftList.Count; i++)
+{
+    sum += Math.Abs(leftList[i] - rightList[i]);
+}
+
+Console.WriteLine(sum);
+
+int probabilityScore = 0;
+foreach (int leftNum in leftList)
+{
+    int count = 0;
+    int value = 0;
+    foreach (int rightNum in rightList)
     {
-        static void Main(string[] args)
+        if (leftNum == rightNum)
         {
-            StreamReader sr = new StreamReader("input.txt");
-            List<int> leftList = new List<int>();
-            List<int> rightList = new List<int>();
-            List<int> differences = new List<int>();
-            List<int> probabilityList = new List<int>();
-
-            string line;
-
-            while ((line = sr.ReadLine()) != null)
-            {
-                leftList.Add(int.Parse(line.Split("   ")[0]));
-                rightList.Add(int.Parse(line.Split("   ")[1]));
-            }
-
-            leftList.Sort();
-            rightList.Sort();
-
-            int sum = 0;
-            for (int i = 0; i < leftList.Count; i++)
-            {
-                sum += Math.Abs(leftList[i] - rightList[i]);
-            }
-
-            Console.WriteLine(sum);
-
-            int probabilityScore = 0;
-            foreach (int leftNum in leftList)
-            {
-                int count = 0;
-                int value = 0;
-                foreach (int rightNum in rightList)
-                {
-                    if (leftNum == rightNum)
-                    {
-                        count++;
-                    }
-                }
-                value = leftNum * count;
-                probabilityScore += value;
-            }
-
-            Console.WriteLine(probabilityScore);
-
+            count++;
         }
     }
+    value = leftNum * count;
+    probabilityScore += value;
 }
+
+Console.WriteLine(probabilityScore);
